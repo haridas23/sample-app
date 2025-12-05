ca9a2700b3c3b945379937e66ba28eef824b1aef
haridas23 <77531395+haridas23@users.noreply.github.com>
2025-12-05 14:06:12 +0530

    Update index.js

--- myapp/index.js
+++ myapp/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js Docker-blue!");
+  res.send("Hello from Node.js Docker-green!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
